28bc60ec96d8cd113f18152981b1627f1252f45a
PythonScratcher <81612672+PythonScratcher@users.noreply.github.com>
2024-01-01 21:26:15 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
 figlet "Banana App Store"
-json_file="$HOME/Banana/apps/list.json"
+
+json_file="/tmp/banana_app_store_list.json"
+curl -o "$json_file" http://mirror.thebananastore.xyz/list.json
 
 if [ ! -f "$json_file" ]; then
     echo "Error: Local JSON file '$json_file' not found."
     exit 1
 fi
 
-
 app_names=($(jq -r '.apps[].name' "$json_file"))
-
 
 selected_app=$(yad --list --title="Banana App Store" --column="Apps" "${app_names[@]}" \
                     --height=300 --button="Install" --button="Cancel" --separator=$'\n')
-
 
 if [ "$?" -eq 252 ] || [ -z "$selected_app" ]; then
     exit 0
 fi
 
-
-selected_installer=$(jq -r --arg selected_app "$selected_app" '.apps[] | select(.name == $selected_app) | .installer' "$json_file")
-
-
-chmod +x "$selected_installer" && "$selected_installer"
+selected_installer_url=$(jq -r --arg selected_app "$selected_app" '.apps[] | select(.name == $selected_app) | .installer' "$json_file")
+install_script="/tmp/install_script.sh"
+wget -O "$install_script" "$selected_installer_url"
+chmod +x "$install_script" && "$install_script"
 
 exit 0
